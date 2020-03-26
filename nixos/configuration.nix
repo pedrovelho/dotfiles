3f92d16b6b6304b7181dafa5e842e1495a3869d8
Pedro Velho <pedro.velho@ryax.tech>
2020-03-26 18:06:43 +0100

    Organize xserver options.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -218,14 +218,12 @@
     layout = "us";
     xkbVariant = "intl";
     xkbOptions = "eurosign:e";
+    # Enable touchpad support.
+    libinput.enable = true;
+    # Enable the KDE Desktop Environment.
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
   };
-
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.pvelho = {
