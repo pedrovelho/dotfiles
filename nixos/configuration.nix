6203fdc7d1dd128663d1d87a16396fb0845c7bbe
Pedro Velho <pedro.velho@ryax.tech>
2020-05-07 03:29:43 +0200

    Upgrade.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,31 +4,38 @@
 
 { config, pkgs, ... }:
 
+
+let
+  extensions = (with pkgs.vscode-extensions; [
+      bbenoist.Nix
+      ms-python.python
+      ms-azuretools.vscode-docker
+      ms-vscode-remote.remote-ssh
+    ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+      name = "remote-ssh-edit";
+      publisher = "ms-vscode-remote";
+      version = "0.47.2";
+      sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+  }];
+  vscode-with-extensions = pkgs.vscode-with-extensions.override {
+      vscodeExtensions = extensions;
+    };
+in
 {
-  # Accept non free packages, needed for skype for instance
+
+  # Accept non free packages, needed for skype, zoom, unrar, etc...
   nixpkgs.config.allowUnfree = true;
 
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./users.nix
     ];
 
   # UEFI with anti-freeze on Dell XPS 15
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.pvelho = {
-    isNormalUser = true;
-    description = "Pedro Velho";
-    extraGroups = [ "wheel" "docker" "networkmanager" "audio" "fuse" ];
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC70aw7DXxnBHhtP8u10ozL9I64wXpLBZbpilGBwyNUPOmUrPi72/p7YKhoUbTK9Sti4JnOcew5ats3UHlrtivQR1HeiE49XquMkzmgLSW2EIVHVTooR2evsgPiCxDRMp5p4LIrgiSP4IpjFLEm23XWP5vWeaJDxdCTA3RHIdp0dYld5wZfPqLKRlcxOG7cuN1TDCYdVD7wTu5Eh8EC+zriGpGVAtxUOQyxJLEBa7mSAvzVi9DwnaG06fInFygLevDahz/qX3ZOv2+DkNKegUQe6ge2KmVbpTW+TV2aCMRQahGi4PT++gmim9bIhU4fQbzBw7AfnftWiy5yljdoCfA7 pvelho@lime.home" ];
-    shell = pkgs.zsh;
-  };
-
-  users.extraUsers.adisbladis = {
-    shell = pkgs.zsh;
-  };
   programs.zsh.enable = true;
 
   fonts = {
@@ -37,8 +44,6 @@
       nerdfonts
     ];
   };
-  hardware.bumblebee.enable = true;
-  hardware.bumblebee.pmMethod = "none";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -60,21 +65,24 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   # Set your time zone.
-   time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Paris";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    # vscode
+    vscode-with-extensions
+
+    # Non free, need allowUnfree set to true
     zoom-us
-    vim
-    gnome3.meld
-    nodejs-12_x
-    
-    # nix_utils
+    unrar
+    skype
+
+    # Nix utils
     nix-prefetch-scripts
     nix-zsh-completions
 
-    # monitoring
+    # Monitoring
     psmisc
     pmutils
     nmap
@@ -85,27 +93,23 @@
     tcpdump
     lsof
 
-    # files
+    # Files
     file
     tree
     ncdu
     unzip
-    unrar #NOT FREE need allowUnfree set to true
 
     # Java
     maven
     jdk
 
-
+    # Command line extra
     bc
     ffmpeg
     hping
     iftop
-    libreoffice
-    qt5.full
-    transmission-gtk
 
-    # tools
+    # Tools
     zsh
     tmux
     ranger
@@ -120,6 +124,7 @@
     # Gnome stuff
     gnomeExtensions.system-monitor
     gnome3.evolution
+    gnome3.gnome-tweaks
     evolution-data-server
     
     # KDE stuff
@@ -128,16 +133,20 @@
     # Web
     firefox
     chrome-gnome-shell
+    nodejs-12_x
+
     # Dictionnaries
     aspellDicts.fr
     aspellDicts.en
+    aspellDicts.pt_BR
+
     # Message and RSS
-    skype
     gnome3.polari
     liferea
 
     # Media
     vlc
+
     # Utils
     gnome3.gnome-disk-utility
     xorg.xkill
@@ -152,16 +161,19 @@
     sysstat
     gsmartcontrol
     linuxPackages.perf
+
     # Password
     gnupg
     rofi-pass
+    mkpasswd
 
     # Graphic tools
+    gnome3.meld
     gcolor3
     graphviz
     imagemagick
     inkscape
-    #libreoffice-fresh
+    libreoffice
     gimp
     gitAndTools.gitFull
     python3
@@ -176,36 +188,45 @@
     entr
     pandoc
     socat
+
+    # Gtk
+    transmission-gtk
+    qt5.full
+
     # Day to day use in Ryax
     cachix
     kubernetes-helm
     kubectl
     k9s
     pssh
+
     # Editors
     emacs
+    vim
+
     # Web Site
     hugo
+
     # Misc
     cloc
     jq
     qemu
-    # printers
+
+    # Printers
     saneBackends
     samsungUnifiedLinuxDriver
-    # fun
+
+    # Fun
     fortune
     sl
     wesnoth-dev
     docker-compose
     sshfsFuse
 
+    # Dell XPS 15 specific
     xorg.xbacklight
-
-    # MS VisuaCode Studio
-    vscode-extensions
   ];
-
+ 
   programs.light.enable = true;
 
   # Enable docker on boot
@@ -217,12 +238,12 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -231,7 +252,7 @@
   # networking.firewall.enable = false;
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # services.printing.enable = true;
 
   # Enable sound.
   sound.enable = true;
@@ -256,18 +277,16 @@
     layout = "us";
     xkbVariant = "intl";
     xkbOptions = "eurosign:e";
+
     # Enable touchpad support.
     libinput.enable = true;
+
     # Enable graphical login and desktop
-    #displayManager.sddm.enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome3.enable = true;
-    #desktopManager.plasma5.enable = true;
     displayManager.defaultSession = "gnome";
   };
-
-  users.mutableUsers = true;
-
+  
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
