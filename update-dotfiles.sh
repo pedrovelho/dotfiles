9546d5cdb1d07bde84f53e117e2d0207febe1e32
Pedro Velho <pedro.velho@ryax.tech>
2024-11-08 08:04:02 +0100

    Script to determine if configuration has changed.

---
+++ update-dotfiles.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+find config -name '*' | while read f ; do
+    if [ $f == "config" ] ; then continue ; fi
+    printf "Analyzing ====> "$f
+    config_file=$(basename $f)
+    DIFF=$(diff ~/$config_file $f 2> /dev/null)
+    if [ $? -ne 0 ] ; then
+        printf " ${RED}CHANGED${NC}\n"
+    else
+        printf " ${GREEN}OK${NC}\n"
+    fi
+done
